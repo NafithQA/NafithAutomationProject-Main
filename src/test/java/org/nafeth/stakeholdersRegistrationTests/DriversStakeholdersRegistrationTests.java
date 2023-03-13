5419faec93dda760a97ac4e7696ba8877b27f7e4
NafithQA <emoqbel@nafith.com>
2023-03-13 16:45:32 +0300

    Adding Tests to DriversStakeholdersRegistrationTests

--- src/test/java/org/nafeth/stakeholdersRegistrationTests/DriversStakeholdersRegistrationTests.java
+++ src/test/java/org/nafeth/stakeholdersRegistrationTests/DriversStakeholdersRegistrationTests.java
@@ -1,9 +1,14 @@
 package org.nafeth.stakeholdersRegistrationTests;
 
 import org.nafeth.base.Configurations;
+import org.nafeth.commonSteps.FleetRegistrationSteps;
+import org.nafeth.commonSteps.GenericSteps;
+import org.nafeth.commonSteps.StakeholderRegistrationSteps;
 import org.nafeth.helpers.Functions;
 import org.nafeth.pageModels.CommonLocators;
+import org.nafeth.pageModels.FleetRegistrationPage;
 import org.nafeth.pageModels.StakeholdersRegistrationPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -34,6 +39,112 @@ public class DriversStakeholdersRegistrationTests extends Configurations {
         commonLocators.clickOnYesButton();
     }
 
-    // Internal driver registration by all Stakeholders + Nafith role
+    @Test(enabled = true)
+    public void Internal_Drivers_Registration_Using_Service_Provider() throws IOException, InterruptedException {
+
+        FleetRegistrationSteps fleetRegistrationSteps = new FleetRegistrationSteps(driver);
+        GenericSteps genericSteps = new GenericSteps(driver);
+        CommonLocators commonLocators = new CommonLocators(driver);
+        StakeholderRegistrationSteps stakeholderRegistrationSteps = new StakeholderRegistrationSteps(driver);
+        StakeholdersRegistrationPage stakeholdersRegistrationPage = new StakeholdersRegistrationPage(driver);
+
+        // Login
+        genericSteps.loginToMAMS(dataLoader.credentialsData("serviceProviderUser"), dataLoader.credentialsData("serviceProviderPass"));
+
+        // Navigate to Fleet Registration Menu
+        fleetRegistrationSteps.navigateToDriverOwnerRegistrationMenu();
+
+        // Fill in Driver Registration Info
+        stakeholderRegistrationSteps.fillInInternalDriverRegistrationInfo();
+
+        // Fill in License Info
+        stakeholderRegistrationSteps.fillInLicenseInfo();
+
+        // Upload Attachments
+        stakeholderRegistrationSteps.uploadDriversRegistrationAttachments();
+
+        // Check the Agreement
+        commonLocators.checkAgreementCheckbox();
+
+        // Proceed with Final Registration Steps
+        stakeholderRegistrationSteps.proceedWithFinalRegistrationSteps();
+
+        // Test Assertion
+        Assert.assertTrue(stakeholdersRegistrationPage.getSuccessRingIcon().isDisplayed());
+
+        Thread.sleep(3000);
+    }
+
+    @Test(enabled = true)
+    public void Internal_Drivers_Registration_Using_Trucking_Company() throws IOException, InterruptedException {
+
+        FleetRegistrationSteps fleetRegistrationSteps = new FleetRegistrationSteps(driver);
+        GenericSteps genericSteps = new GenericSteps(driver);
+        CommonLocators commonLocators = new CommonLocators(driver);
+        StakeholderRegistrationSteps stakeholderRegistrationSteps = new StakeholderRegistrationSteps(driver);
+        StakeholdersRegistrationPage stakeholdersRegistrationPage = new StakeholdersRegistrationPage(driver);
+
+        // Login
+        genericSteps.loginToMAMS(dataLoader.credentialsData("truckingCompanyUser"), dataLoader.credentialsData("truckingCompanyPass"));
+
+        // Navigate to Fleet Registration Menu
+        fleetRegistrationSteps.navigateToDriverOwnerRegistrationMenu();
+
+        // Fill in Driver Registration Info
+        stakeholderRegistrationSteps.fillInInternalDriverRegistrationInfo();
+
+        // Fill in License Info
+        stakeholderRegistrationSteps.fillInLicenseInfo();
+
+        // Upload Attachments
+        stakeholderRegistrationSteps.uploadDriversRegistrationAttachments();
+
+        // Check the Agreement
+        commonLocators.checkAgreementCheckbox();
+
+        // Proceed with Final Registration Steps
+        stakeholderRegistrationSteps.proceedWithFinalRegistrationSteps();
+
+        // Test Assertion
+        Assert.assertTrue(stakeholdersRegistrationPage.getSuccessRingIcon().isDisplayed());
+
+        Thread.sleep(3000);
+    }
+
+    @Test(enabled = true)
+    public void Internal_Drivers_Registration_Using_Investor() throws IOException, InterruptedException {
+
+        FleetRegistrationSteps fleetRegistrationSteps = new FleetRegistrationSteps(driver);
+        GenericSteps genericSteps = new GenericSteps(driver);
+        CommonLocators commonLocators = new CommonLocators(driver);
+        StakeholderRegistrationSteps stakeholderRegistrationSteps = new StakeholderRegistrationSteps(driver);
+        StakeholdersRegistrationPage stakeholdersRegistrationPage = new StakeholdersRegistrationPage(driver);
+
+        // Login
+        genericSteps.loginToMAMS(dataLoader.credentialsData("investorUser"), dataLoader.credentialsData("investorPass"));
+
+        // Navigate to Fleet Registration Menu
+        fleetRegistrationSteps.navigateToDriverOwnerRegistrationMenu_Investor();
+
+        // Fill in Driver Registration Info
+        stakeholderRegistrationSteps.fillInInternalDriverRegistrationInfo();
+
+        // Fill in License Info
+        stakeholderRegistrationSteps.fillInLicenseInfo();
+
+        // Upload Attachments
+        stakeholderRegistrationSteps.uploadDriversRegistrationAttachments();
+
+        // Check the Agreement
+        commonLocators.checkAgreementCheckbox();
+
+        // Proceed with Final Registration Steps
+        stakeholderRegistrationSteps.proceedWithFinalRegistrationSteps();
+
+        // Test Assertion
+        Assert.assertTrue(stakeholdersRegistrationPage.getSuccessRingIcon().isDisplayed());
+
+        Thread.sleep(3000);
+    }
 }
 
