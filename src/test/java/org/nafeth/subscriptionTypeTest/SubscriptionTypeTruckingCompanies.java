0af19f5eda9c86d3468fa56831d226013af4e124
NafithQA <123379692+NafithQA@users.noreply.github.com>
2023-03-29 11:57:58 +0300

    Adding New Functions To Subscription Type Class

--- src/test/java/org/nafeth/subscriptionTypeTest/SubscriptionTypeTruckingCompanies.java
+++ src/test/java/org/nafeth/subscriptionTypeTest/SubscriptionTypeTruckingCompanies.java
@@ -2,44 +2,41 @@ package org.nafeth.subscriptionTypeTest;
 
 import org.nafeth.base.Configurations;
 import org.nafeth.commonSteps.GenericSteps;
-import org.nafeth.commonSteps.StakeholderRegistrationSteps;
-import org.nafeth.pageModels.StakeholdersRegistrationPage;
+import org.nafeth.commonSteps.SubscriptionTypeSteps;
+import org.nafeth.pageModels.SubscriptionTypePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-public class SubscriptionTypeTruckingCompanies  extends Configurations {
+public class SubscriptionTypeTruckingCompanies extends Configurations {
 
     @Test(enabled = true)
     public void SubscriptionType_TruckingCompanies() throws IOException, InterruptedException {
 
-        StakeholdersRegistrationPage stakeholdersRegistrationPage = new StakeholdersRegistrationPage(driver);
-        StakeholderRegistrationSteps stakeholderRegistrationSteps = new StakeholderRegistrationSteps(driver);
+        SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
+        SubscriptionTypeSteps subscriptionTypeSteps = new SubscriptionTypeSteps(driver);
         GenericSteps genericSteps = new GenericSteps(driver);
 
         // Login
-        genericSteps.loginToMAMS(dataLoader.credentialsData("systemUser"), dataLoader.credentialsData("systemPass"));
+        genericSteps.loginToMAMS(dataLoader.credentialsData("truckingCompanyUser"), dataLoader.credentialsData("truckingCompanyPass"));
 
-        // Navigate to Stakeholder Registration Menu
-        stakeholderRegistrationSteps.navigateToStakeholderRegistrationMenu();
+        // Navigate to Fleet Subscription Menu
+        subscriptionTypeSteps.navigateToFleetSubscriptionMenu();
 
-        // Navigate to Investors Registration Menu
-        stakeholderRegistrationSteps.navigateToInvestorsRegistrationMenu();
+        // Navigate To Packages
+        subscriptionTypeSteps.navigateToPackages();
 
-        // Fill-In Investors Registration Info
-        stakeholderRegistrationSteps.fillInInvestorsRegistrationInfo();
+        // Create  Fleet Subscription
+        subscriptionTypeSteps.createFleetSubscription();
 
-        // Fill-In  Parking Capacity tab
-        stakeholderRegistrationSteps.fillParkingCapacityRegistrationInfo();
-
-        // Upload Attachments
-        stakeholderRegistrationSteps.uploadStakeholderRegistrationAttachments();
+        // Check the Agreement
+        subscriptionTypePage.clickOnAgreementCheckbox();
 
         // Proceed with Final Registration Steps
-        stakeholderRegistrationSteps.proceedWithFinalRegistrationSteps();
+        subscriptionTypeSteps.proceedWithFinalRegistrationSteps();
 
         // Test Assertion
-        Assert.assertTrue(stakeholdersRegistrationPage.getSuccessRingIcon().isDisplayed());
+        Assert.assertTrue(subscriptionTypePage.getSuccessRingIcon().isDisplayed());
     }
 }
