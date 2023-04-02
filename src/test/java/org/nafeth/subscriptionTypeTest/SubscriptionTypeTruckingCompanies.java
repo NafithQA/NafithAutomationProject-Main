aeb6f78305719b12c0d72414e6e44ea526b8e8ac
NafithQA <123379692+NafithQA@users.noreply.github.com>
2023-04-02 10:09:48 +0300

    Restructuring SubscriptionType_TruckingCompanies_HeavyTruck_FromBalance And Adding New Function

--- src/test/java/org/nafeth/subscriptionTypeTest/SubscriptionTypeTruckingCompanies.java
+++ src/test/java/org/nafeth/subscriptionTypeTest/SubscriptionTypeTruckingCompanies.java
@@ -12,7 +12,36 @@ import java.io.IOException;
 public class SubscriptionTypeTruckingCompanies extends Configurations {
 
     @Test(enabled = true)
-    public void SubscriptionType_TruckingCompanies() throws IOException, InterruptedException {
+    public void SubscriptionType_TruckingCompanies_HeavyTruck_FromBalance() throws IOException, InterruptedException {
+
+        SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
+        SubscriptionTypeSteps subscriptionTypeSteps = new SubscriptionTypeSteps(driver);
+        GenericSteps genericSteps = new GenericSteps(driver);
+
+        // Login
+        genericSteps.loginToMAMS(dataLoader.credentialsData("truckingCompanyUser"), dataLoader.credentialsData("truckingCompanyPass"));
+
+        // Navigate to Fleet Subscription Menu
+        subscriptionTypeSteps.navigateToFleetSubscriptionMenu();
+
+        // Navigate To Packages
+        subscriptionTypeSteps.navigateToPackages();
+
+        // Create  Fleet Subscription
+        subscriptionTypeSteps.createFleetSubscription();
+
+        // Check the Agreement
+        subscriptionTypePage.clickOnAgreementCheckbox();
+
+        // Proceed with Final Registration Steps
+        subscriptionTypeSteps.proceedWithFinalRegistrationSteps();
+
+        // Test Assertion
+        Assert.assertTrue(subscriptionTypePage.getSuccessRingIcon().isDisplayed());
+    }
+
+    @Test(enabled = true)
+    public void SubscriptionType_TruckingCompanies_Buses_POS() throws IOException, InterruptedException {
 
         SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
         SubscriptionTypeSteps subscriptionTypeSteps = new SubscriptionTypeSteps(driver);
