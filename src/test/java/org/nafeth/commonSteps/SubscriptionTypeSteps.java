0c3e0eb0e4f8ee0a4bd7d2ca7f7047672ca61d0b
NafithQA <123379692+NafithQA@users.noreply.github.com>
2023-04-16 15:13:36 +0300

    Adding Permit Functions

--- src/test/java/org/nafeth/commonSteps/SubscriptionTypeSteps.java
+++ src/test/java/org/nafeth/commonSteps/SubscriptionTypeSteps.java
@@ -1,11 +1,14 @@
 package org.nafeth.commonSteps;
 
+import org.nafeth.helpers.DatabaseHandler;
 import org.nafeth.helpers.Functions;
+import org.nafeth.helpers.Queries.OtherQueries;
 import org.nafeth.pageModels.CommonLocators;
 import org.nafeth.pageModels.HomePage;
-import org.nafeth.pageModels.StakeholdersRegistrationPage;
 import org.nafeth.pageModels.SubscriptionTypePage;
 import org.openqa.selenium.WebDriver;
+
+import java.util.ArrayList;
 
 public class SubscriptionTypeSteps {
 
@@ -29,41 +32,132 @@ public class SubscriptionTypeSteps {
         functions.scrollThePageDown();
         homePage.clickOnCreateFleetSubscriptionsButton();
         Thread.sleep(8000);
-        commonLocators.clickOnOwnedFleetChooseButton();
     }
 
-    public void navigateToPackages() throws InterruptedException {
+    public void navigateToPermitPackages() throws InterruptedException {
 
         CommonLocators commonLocators = new CommonLocators(driver);
 
+        commonLocators.clickOnOneTimePermitButton();
+        commonLocators.clickOnOneTimePermitForHeavyTrucksButton();
+        commonLocators.clickOnAccessServiceChooseButton();
+        commonLocators.clickOnOneTimePermitAccess();
+        Thread.sleep(1000);
+    }
+
+    public void navigateToHeavyTrucksPackages() throws InterruptedException {
+
+        CommonLocators commonLocators = new CommonLocators(driver);
+
+        commonLocators.clickOnOwnedFleetChooseButton();
         commonLocators.clickOnFirstAvailablePackageChooseButton();
         commonLocators.clickOnAccessServiceChooseButton();
         Thread.sleep(1000);
     }
 
-    public void createFleetSubscription() throws InterruptedException {
+    public void navigateToBusPackages() throws InterruptedException {
+
+        CommonLocators commonLocators = new CommonLocators(driver);
+
+        commonLocators.clickOnOwnedFleetChooseButton();
+        commonLocators.clickOnBusPackageChooseButton();
+        commonLocators.clickOnAccessServiceChooseButton();
+        Thread.sleep(1000);
+    }
+
+    public void createHeavyFleetSubscription() throws InterruptedException {
 
         Functions functions = new Functions();
         SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
 
         functions.dropDownPickerByIndex(subscriptionTypePage.getIndustrialEstateDropDownList(), 1);
         subscriptionTypePage.clickOnPackageOneMonthButton();
-        subscriptionTypePage.clickOnchooseAnyTruck();
+        subscriptionTypePage.clickOnChooseAnyVehicle();
         subscriptionTypePage.clickOnSelectOneTruckToSubscribe();
         functions.scrollIntoElement(subscriptionTypePage.getNextButton());
         subscriptionTypePage.clickOnNextButton();
         Thread.sleep(750);
         functions.scrollThePageDown();
-        subscriptionTypePage.selectBalanceoption();
+    }
+
+    public void payByVISA() throws InterruptedException {
+        SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
+
+        subscriptionTypePage.selectVISAOption();
         Thread.sleep(750);
     }
+
+    public void payByPOS() throws InterruptedException {
+        SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
+        DatabaseHandler databaseHandler = new DatabaseHandler();
+
+        subscriptionTypePage.selectPOSOption();
+        ArrayList<String> testData = (databaseHandler.getValidRnnFromDataBase(OtherQueries.dynamicRnnQuery("10")));
+        String rnn = testData.get(0);
+        subscriptionTypePage.getReceiptIdField().sendKeys(rnn);
+        Thread.sleep(750);
+    }
+
+    public void payByBalance() throws InterruptedException {
+        SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
+
+        subscriptionTypePage.selectBalanceOption();
+        Thread.sleep(750);
+    }
+
+    public void createBusesFleetSubscription() throws InterruptedException {
+
+        SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
+        Functions functions = new Functions();
+
+        functions.dropDownPickerByIndex(subscriptionTypePage.getIndustrialEstateDropDownList(), 1);
+        subscriptionTypePage.clickOnPackageBusOneWeekButton();
+        subscriptionTypePage.clickOnChooseAnyVehicle();
+        subscriptionTypePage.clickOnSelectOneTruckToSubscribe();
+        functions.scrollIntoElement(subscriptionTypePage.getNextButton());
+        subscriptionTypePage.clickOnNextButton();
+        Thread.sleep(750);
+        functions.scrollThePageDown();
+    }
+
     public void proceedWithFinalRegistrationSteps() throws InterruptedException {
 
         Functions functions = new Functions();
         CommonLocators commonLocators = new CommonLocators(driver);
-        SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
 
         functions.scrollThePageDown();
-        subscriptionTypePage.clickOnPayButton();
+        functions.scrollIntoElementAndClickOnIt(commonLocators.getPayButton());
+        Thread.sleep(750);
+        commonLocators.clickOnYesButton();
+
+    }
+
+    public void createOneTimePermitSubscription() throws InterruptedException {
+
+        Functions functions = new Functions();
+        SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
+
+
+        String ApprovalNumber = String.valueOf("FLEET_APPROVAL_NUMBER" + functions.getRandomInvestorName());
+        //  subscriptionTypePage.fillArabicName(ApprovalNumber);
+        functions.dropDownPickerByIndex(subscriptionTypePage.getIndustrialEstateDropDownList(), 1);
+        subscriptionTypePage.clickOnPackageOneMonthButton();
+        subscriptionTypePage.clickOnChooseAnyVehicle();
+        subscriptionTypePage.clickOnSelectOneTruckToSubscribe();
+        functions.scrollIntoElement(subscriptionTypePage.getNextButton());
+        subscriptionTypePage.clickOnNextButton();
+        Thread.sleep(750);
+        functions.scrollThePageDown();
+    }
+
+    public void ApprovalNumber() throws InterruptedException {
+        SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
+        DatabaseHandler databaseHandler = new DatabaseHandler();
+
+        subscriptionTypePage.fillApprovalNumber();
+        ArrayList<String> testData = (databaseHandler.getValidRnnFromDataBase(OtherQueries.dynamicRnnQuery("10")));
+        String rnn = testData.get(0);
+        subscriptionTypePage.getReceiptIdField().sendKeys(rnn);
+        Thread.sleep(750);
     }
 }
