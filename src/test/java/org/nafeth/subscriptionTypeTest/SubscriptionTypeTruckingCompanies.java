e43dc54694eb9de1efe3390d7dd5b70d0d645163
NafithQA <123379692+NafithQA@users.noreply.github.com>
2023-04-16 15:20:07 +0300

    Updating   subscription Type

--- src/test/java/org/nafeth/subscriptionTypeTest/SubscriptionTypeTruckingCompanies.java
+++ src/test/java/org/nafeth/subscriptionTypeTest/SubscriptionTypeTruckingCompanies.java
@@ -3,6 +3,7 @@ package org.nafeth.subscriptionTypeTest;
 import org.nafeth.base.Configurations;
 import org.nafeth.commonSteps.GenericSteps;
 import org.nafeth.commonSteps.SubscriptionTypeSteps;
+import org.nafeth.pageModels.CreditPaymentPage;
 import org.nafeth.pageModels.SubscriptionTypePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -24,21 +25,24 @@ public class SubscriptionTypeTruckingCompanies extends Configurations {
         // Navigate to Fleet Subscription Menu
         subscriptionTypeSteps.navigateToFleetSubscriptionMenu();
 
-        // Navigate To Packages
-        subscriptionTypeSteps.navigateToPackages();
+        // Navigate To Heavy Trucks Packages
+        subscriptionTypeSteps.navigateToHeavyTrucksPackages();
 
         // Create  Fleet Subscription
-        subscriptionTypeSteps.createFleetSubscription();
+        subscriptionTypeSteps.createHeavyFleetSubscription();
+
+        // Pay By Balance
+        subscriptionTypeSteps.payByBalance();
 
         // Check the Agreement
         subscriptionTypePage.clickOnAgreementCheckbox();
 
         // Proceed with Final Registration Steps
         subscriptionTypeSteps.proceedWithFinalRegistrationSteps();
-
-        // Test Assertion
+         // Test Assertion
         Assert.assertTrue(subscriptionTypePage.getSuccessRingIcon().isDisplayed());
-    }
+
+     }
 
     @Test(enabled = true)
     public void SubscriptionType_TruckingCompanies_Buses_POS() throws IOException, InterruptedException {
@@ -53,11 +57,46 @@ public class SubscriptionTypeTruckingCompanies extends Configurations {
         // Navigate to Fleet Subscription Menu
         subscriptionTypeSteps.navigateToFleetSubscriptionMenu();
 
-        // Navigate To Packages
-        subscriptionTypeSteps.navigateToPackages();
+        // Navigate To Bus Packages
+        subscriptionTypeSteps.navigateToBusPackages();
 
         // Create  Fleet Subscription
-        subscriptionTypeSteps.createFleetSubscription();
+        subscriptionTypeSteps.createBusesFleetSubscription();
+
+        // Pay By POS
+        subscriptionTypeSteps.payByPOS();
+
+        // Check the Agreement
+        subscriptionTypePage.clickOnAgreementCheckbox();
+
+        // Proceed with Final Registration Steps
+        subscriptionTypeSteps.proceedWithFinalRegistrationSteps();
+
+        // Test Assertion
+        Assert.assertTrue(subscriptionTypePage.getSuccessRingIcon().isDisplayed());
+    }
+
+    @Test(enabled = false)
+    public void SubscriptionType_TruckingCompanies_HeavyTruck_FromVISA() throws IOException, InterruptedException {
+
+        SubscriptionTypePage subscriptionTypePage = new SubscriptionTypePage(driver);
+        SubscriptionTypeSteps subscriptionTypeSteps = new SubscriptionTypeSteps(driver);
+        GenericSteps genericSteps = new GenericSteps(driver);
+
+        // Login
+        genericSteps.loginToMAMS(dataLoader.credentialsData("truckingCompanyUser"), dataLoader.credentialsData("truckingCompanyPass"));
+
+        // Navigate to Fleet Subscription Menu
+        subscriptionTypeSteps.navigateToFleetSubscriptionMenu();
+
+        // Navigate To Heavy Trucks Packages
+        subscriptionTypeSteps.navigateToHeavyTrucksPackages();
+
+        // Create  Fleet Subscription
+        subscriptionTypeSteps.createHeavyFleetSubscription();
+
+        // Pay By VISA
+          subscriptionTypeSteps.payByVISA();
 
         // Check the Agreement
         subscriptionTypePage.clickOnAgreementCheckbox();
